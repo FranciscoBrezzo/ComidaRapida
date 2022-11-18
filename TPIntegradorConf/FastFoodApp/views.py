1ecffe8cdbb2720e9008ece0bc84672b61553527
FranciscoBrezzo <fran.brezzo@gmail.com>
2022-11-18 11:53:04 -0300

    creacion de los archivos order.html y status.html

--- TPIntegradorConf/FastFoodApp/views.py
+++ TPIntegradorConf/FastFoodApp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def order(request):
-    return render(request,'FastFoodApp/base.html')
+    return render(request,'FastFoodApp/order.html')
 
 def status(request):
-    return HttpResponse('I am the status view')
+    return render(request,'FastFoodApp/status.html')
