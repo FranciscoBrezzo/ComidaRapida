aad8c6148c8534199b4aa72495eb8ac89ca0fdfc
Bautista Cimatti <bcimatti2005@gmail.com>
2022-11-18 10:29:24 -0300

    order y status views

---
+++ TPIntegradorConf/FastFoodApp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'FastFoodApp'
+
+urlpatterns = [
+    path('order/', views.order, name='order'),
+    path('status/', views.status, name='status'),
+]
--- TPIntegradorConf/FastFoodApp/views.py
+++ TPIntegradorConf/FastFoodApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def order(request):
+    return HttpResponse('I am the order view')
+
+def status(request):
+    return HttpResponse('I am the status view')
