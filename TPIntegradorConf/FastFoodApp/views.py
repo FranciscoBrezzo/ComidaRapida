6e1e2c9e8e60c1c20e850066005f842271639d9e
FranciscoBrezzo <fran.brezzo@gmail.com>
2022-11-24 18:30:17 -0300

    creacion de tabla para registrar venta

--- TPIntegradorConf/FastFoodApp/views.py
+++ TPIntegradorConf/FastFoodApp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Menu
+from .models import Producto
 
 # Create your views here.
 def order(request):
-    return render(request,'FastFoodApp/order.html')
+    menus = Menu.objects.all()
+    productos = Producto.objects.all()
+    return render(request,'FastFoodApp/order.html', {"menus" : menus, "productos" : productos})
 
 def status(request):
     return render(request,'FastFoodApp/status.html')
