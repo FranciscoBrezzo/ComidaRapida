2e58dea95313dec3f460ad6fd5600b3f01efa89a
JuanIgnacioGonzalez <juanignaciogonzalez.6c@gmail.com>
2022-11-25 10:00:54 -0300

    homepage

--- TPIntegradorConf/FastFoodApp/urls.py
+++ TPIntegradorConf/FastFoodApp/urls.py
@@ -6,7 +6,4 @@ app_name = 'FastFoodApp'
 urlpatterns = [
     path('order/', views.order, name='order'),
     path('status/', views.status, name='status'),
-    
-
-
 ]
--- TPIntegradorConf/FastFoodApp/views.py
+++ TPIntegradorConf/FastFoodApp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
-
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
-
+    
 # Create your views here.
 def order(request):
     return HttpResponse('I am the order view')
 
 def status(request):
-    return HttpResponse('I am the status view')
+        return HttpResponse('I am the status view')
