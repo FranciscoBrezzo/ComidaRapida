4165ebd289f3e101c15d8bd58dc6182258d20a36
FranciscoBrezzo <fran.brezzo@gmail.com>
2022-11-28 19:33:34 -0300

    actualizacion del template order.html

--- TPIntegradorConf/FastFoodApp/static/FastFoodApp/js/order.js
+++ TPIntegradorConf/FastFoodApp/static/FastFoodApp/js/order.js
@@ -1,2 +1,74 @@
-let valor = document.getElementById("input_cantidad")
+function CalcularMontoTotal(){
+    let cantidad = document.getElementById("input_c").value;
+    let precioUnitario = document.getElementById("label_monto").innerText;
+    var checkbox = document.querySelector("input[name=checkbox]");
+    var checked = checkbox.checked;
+    if(checked){
+        let total = cantidad * precioUnitario
+        console.log("precioUnitario:"+precioUnitario)
+        console.log("cantidad:"+cantidad)
+        console.log("total:"+total)
+        cantidad = 0
+        precioUnitario = 0
+        document.getElementById('MontoTotal').innerHTML = total
+
+    }
+    else{
+        console.log("0")
+    }
+}
+
+function SoloNumeros(evt){
+    var code = (evt.which) ? evt.which : evt.keyCode;
+    
+    if(code==8) { 
+      return true;
+    } else if(code>=48 && code<=57) { 
+      return true;
+    } else{ 
+      return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+let cantidad = document.getElementById("input_cantidad")
+let precioUnitario = document.getElementById("labe_monto")
+
+var checkbox = document.querySelector("input[name=checkbox]");
+
+
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+      console.log("Checkbox is checked..");
+    } else {
+      console.log("Checkbox is not checked..");
+    }
+  });
+*/
+
+/*
+checkbox.addEventListener("change", validaCheckbox, false);
+function validaCheckbox(){
+    var checked = checkbox.checked;
+    if (checked) {
+        montoTotal = cantidad * precioUnitario
+        console.log(montoTotal);
+    } else {
+        console.log(0);
+    }
+};
+*/
+
+
+
+
 
--- TPIntegradorConf/FastFoodApp/views.py
+++ TPIntegradorConf/FastFoodApp/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 from .models import Menu
 from .models import Producto
+
 
 # Create your views here.
 def order(request):
     menus = Menu.objects.all()
     productos = Producto.objects.all()
+    busqueda = request.GET.get("buscar")
+    
+    if busqueda:
+        menus = Menu.objects.filter(
+            Q(nombre__icontains = busqueda) |
+            Q(precio__icontains = busqueda)
+        ).distinct()
     return render(request,'FastFoodApp/order.html', {"menus" : menus, "productos" : productos})
 
 def status(request):
